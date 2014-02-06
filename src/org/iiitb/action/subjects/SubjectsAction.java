6752c01f6a3e0b134730315665811ed8c19df2f1
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-02-06 21:50:49 +0530

    Initial changes related to Subjects Action

---
+++ src/org/iiitb/action/subjects/SubjectsAction.java
@@ -0,0 +1,13 @@
+package org.iiitb.action.subjects;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SubjectsAction extends ActionSupport{
+
+  /**
+   * serial id
+   */
+  private static final long serialVersionUID = 3193702317572110684L;
+
+  
+}

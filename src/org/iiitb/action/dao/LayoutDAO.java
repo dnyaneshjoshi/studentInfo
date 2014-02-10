a994e6b2200b317c20d62cdc560f3a12c7700991
Dnyanesh Joshi <dnyanesh.joshi@iiitb.org>
2014-02-10 23:05:36 +0530

    first commit

---
+++ src/org/iiitb/action/dao/LayoutDAO.java
@@ -0,0 +1,14 @@
+package org.iiitb.action.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.iiitb.model.layout.AnnouncementsItem;
+import org.iiitb.model.layout.NewsItem;
+
+public interface LayoutDAO
+{
+	public List<NewsItem> getAllNews(Connection connection) throws SQLException;
+	public List<AnnouncementsItem> getAnnouncements(Connection connection, int userId) throws SQLException;
+}

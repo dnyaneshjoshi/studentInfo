4ef7363f2d2286307d2cb8f1ef5de67bab5905f5
harikrishnam <mharikrishna0512@gmail.com>
2014-02-28 18:51:38 +0530

    Unstable commit

---
+++ src/org/iiitb/action/dao/UpdateGradesAction.java
@@ -0,0 +1,156 @@
+package org.iiitb.action.dao;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+import org.iiitb.action.dao.CourseDAO;
+import org.iiitb.action.dao.LayoutDAO;
+import org.iiitb.action.dao.ResultDAO;
+import org.iiitb.action.dao.impl.CourseDAOImpl;
+import org.iiitb.action.dao.impl.LayoutDAOImpl;
+import org.iiitb.action.dao.impl.ResultDAOImpl;
+import org.iiitb.action.dao.impl.SemesterDAOImpl;
+import org.iiitb.action.dao.impl.StudentDAOImpl;
+import org.iiitb.action.dao.SemesterDAO;
+import org.iiitb.model.User;
+import org.iiitb.model.layout.AnnouncementsItem;
+import org.iiitb.model.layout.NewsItem;
+import org.iiitb.util.ConnectionPool;
+import org.iiitb.util.Constants;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 
+ * @author Abhijith Madhav (MT2013002)
+ * 
+ */
+public class UpdateGradesAction extends ActionSupport implements SessionAware
+{
+	/**
+	 * 
+	 * serial id
+	 */
+	private static final long serialVersionUID = -3927650660405287420L;
+
+	private List<String> studentList;
+	private List<String> courseList;
+	private List<String> gradeList;
+
+	private Map<String, Object> session;
+	private static final String USER = "user";
+
+	private List<NewsItem> allNews;
+	private List<AnnouncementsItem> announcements;
+	private LayoutDAO layoutDAO = new LayoutDAOImpl();
+	private String lastLoggedOn = "";
+
+	private int studentDisplayChoice;
+
+	public UpdateGradesAction()
+	{
+		studentList = new LinkedList<String>();
+		//studentList.add(DEFAULT_TERM);
+
+		courseList = new LinkedList<String>();
+		//courseList.add(DEFAULT_COURSE);
+
+		//setResultList(new LinkedList<GradeInfo>());
+		gradeList = new LinkedList<String>();
+	}
+
+	public String execute() throws Exception
+	{
+		User loggedInUser = (User) this.session.get(USER);
+		if (loggedInUser != null)
+		{	
+			Connection connection = ConnectionPool.getConnection();
+			studentList.addAll(new StudentDAOImpl().getStudents());
+
+			courseList.addAll(new CourseDAOImpl().getEnrolledCourses(connection, studentDisplayChoice));
+			gradeList.addAll(new ResultDAOImpl().getGrades());
+			
+			allNews = layoutDAO.getAllNews(connection);
+			announcements = layoutDAO.getAnnouncements(connection,
+					Integer.parseInt(loggedInUser.getUserId()));
+			setLastLoggedOn((String) this.session.get(Constants.LAST_LOGGED_ON));
+			ConnectionPool.freeConnection(connection);
+			return SUCCESS;
+		}
+		else
+			return LOGIN;
+	}
+
+
+	public List<String> getCourseList()
+	{
+		return courseList;
+	}
+
+	public void setCourseList(List<String> courseList)
+	{
+		this.courseList = courseList;
+	}
+
+	public List<NewsItem> getAllNews()
+	{
+		return allNews;
+	}
+
+	public void setAllNews(List<NewsItem> allNews)
+	{
+		this.allNews = allNews;
+	}
+
+	public List<AnnouncementsItem> getAnnouncements()
+	{
+		return announcements;
+	}
+
+	public void setAnnouncements(List<AnnouncementsItem> announcements)
+	{
+		this.announcements = announcements;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.apache.struts2.interceptor.SessionAware#setSession(java.util.Map)
+	 */
+	@Override
+	public void setSession(Map<String, Object> session)
+	{
+		this.session = session;
+
+	}
+
+	public String getLastLoggedOn()
+	{
+		return lastLoggedOn;
+	}
+
+	public void setLastLoggedOn(String lastLoggedOn)
+	{
+		this.lastLoggedOn = lastLoggedOn;
+	}
+
+	public List<String> getStudentList() {
+		return studentList;
+	}
+
+	public void setStudentList(List<String> studentList) {
+		this.studentList = studentList;
+	}
+
+	public List<String> getGradeList() {
+		return gradeList;
+	}
+
+	public void setGradeList(List<String> gradeList) {
+		this.gradeList = gradeList;
+	}
+}

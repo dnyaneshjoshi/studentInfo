5d2f9db2af23af62ae7ef0a95665e3ef4c6e5f4d
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-03-01 10:30:23 +0530

    Announcements and News in Enrollment.

--- src/org/iiitb/action/enrollment/EnrollmentAction.java
+++ src/org/iiitb/action/enrollment/EnrollmentAction.java
@@ -1,14 +1,19 @@
 package org.iiitb.action.enrollment;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
+import org.iiitb.action.dao.LayoutDAO;
+import org.iiitb.action.dao.impl.LayoutDAOImpl;
 import org.iiitb.action.dao.impl.SemesterDAOImpl;
 import org.iiitb.model.User;
+import org.iiitb.model.layout.AnnouncementsItem;
+import org.iiitb.model.layout.NewsItem;
+import org.iiitb.util.ConnectionPool;
 import org.iiitb.util.Constants;
-
 import com.opensymphony.xwork2.ActionSupport;
 
 public class EnrollmentAction extends ActionSupport implements SessionAware {
@@ -21,7 +26,10 @@ public class EnrollmentAction extends ActionSupport implements SessionAware {
   private Map<String, Object> session;
 
   private List<String> semester;
-
+  private List<NewsItem> allNews;
+  private List<AnnouncementsItem> announcements;
+  private LayoutDAO layoutDAO = new LayoutDAOImpl();
+  
   public List<String> getSemester() {
     return semester;
   }
@@ -37,6 +45,11 @@ public class EnrollmentAction extends ActionSupport implements SessionAware {
     if (null != loggedInUser) {
       semester = new SemesterDAOImpl().getTerms();
       setLastLoggedOn((String) this.session.get(Constants.LAST_LOGGED_ON));
+      Connection connection = ConnectionPool.getConnection();
+      allNews = layoutDAO.getAllNews(connection);
+      announcements = layoutDAO.getAnnouncements(connection,
+          Integer.parseInt(loggedInUser.getUserId()));
+      ConnectionPool.freeConnection(connection);
       return SUCCESS;
     } else {
       return LOGIN;
@@ -54,6 +67,22 @@ public class EnrollmentAction extends ActionSupport implements SessionAware {
 
   public void setLastLoggedOn(String lastLoggedOn) {
     this.lastLoggedOn = lastLoggedOn;
+  }
+
+  public List<NewsItem> getAllNews() {
+    return allNews;
+  }
+
+  public void setAllNews(List<NewsItem> allNews) {
+    this.allNews = allNews;
+  }
+
+  public List<AnnouncementsItem> getAnnouncements() {
+    return announcements;
+  }
+
+  public void setAnnouncements(List<AnnouncementsItem> announcements) {
+    this.announcements = announcements;
   }
 
 }
